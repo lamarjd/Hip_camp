a29e071f29490c06eb82a7badd35d62306cbe80e
lamarjd <jacob.lamar16@gmail.com>
2022-11-23 15:20:29 -0800

    removed conditional in versions file

--- migrations/versions/20221122_210856_init_migrate.py
+++ migrations/versions/20221122_210856_init_migrate.py
@@ -7,9 +7,6 @@ Create Date: 2022-11-22 21:08:56.815194
 """
 from alembic import op
 import sqlalchemy as sa
-import os
-environment = os.getenv('FLASK_ENV')
-SCHEMA = os.environ.get('SCHEMA')
 
 
 # revision identifiers, used by Alembic.
@@ -35,9 +32,6 @@ def upgrade():
     sa.UniqueConstraint('username')
     )
 
-    if environment == 'production':
-        op.execute(f'ALTER TABLE users SET SCHEMA {SCHEMA};') 
-
     op.create_table('spots',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=100), nullable=False),
@@ -54,9 +48,6 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
 
-    if environment == 'production':
-        op.execute(f'ALTER TABLE spots SET SCHEMA {SCHEMA};') 
-
     op.create_table('reviews',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('spot_id', sa.Integer(), nullable=True),
@@ -70,8 +61,7 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     
-    if environment == 'production':
-        op.execute(f'ALTER TABLE reviews SET SCHEMA {SCHEMA};') 
+   
     # ### end Alembic commands ###
 
 
