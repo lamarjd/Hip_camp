a7caf6cebfa29ad2f4bc6e94c643a44f127224c0
lamarjd <jacob.lamar16@gmail.com>
2022-11-22 20:04:13 -0800

    last send for version file

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -15,6 +16,9 @@ class User(db.Model, UserMixin):
     hashed_password = db.Column(db.String(255), nullable=False)
     first_name = db.Column(db.String(30), nullable = False)
     last_name = db.Column(db.String(30), nullable = False)
+    created_at = db.Column(db.DateTime, default= datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default= datetime.utcnow)
+    
 
     @property
     def password(self):
