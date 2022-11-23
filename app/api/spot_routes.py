0392bd18141522cfa3a261e816440c647fa02071
lamarjd <jacob.lamar16@gmail.com>
2022-11-22 22:08:02 -0800

    spots seeded, but cannot retrieve from Postman

--- app/api/spot_routes.py
+++ app/api/spot_routes.py
@@ -1,18 +1,31 @@
 from flask import Blueprint, jsonify, render_template, request, redirect, Response, make_response
 from flask_login import login_required, current_user
-from app.models import Task, db
+from app.models import Spot, db
 from app.forms.spot_form import NewSpot
 
 #import models
-from ..models import Spot, User
+from ..models import Spot, User, Review
 
 spot_routes = Blueprint('spots', __name__)
 
-#  get all tasks
-@spot_routes.route('/spots')
+#  get all spots
+@spot_routes.route('/')
 
 def get_all_spots():
     # if current_user.is_authenticated:
         spots = Spot.query.all()
-        response = {"spots": [spot.to_dict() for spot in spots]}
-        return make_response(response, 200)
+        return {"spots": [spot.to_dict() for spot in spots]}
+        # return make_response(response, 200)
+
+
+@spot_routes.route('<int:id>')
+def get_one_spot(id):
+    spot = Spot.query.get(id)
+    if not spot:
+        return make_response("Doesn't Exist", 404)
+    new_spot = spot.to_dict()
+    spot_reviews = Review.query.filter(Review.spot_id == id).all()
+    something = [review.to_dict() for review in spot_reviews]
+    new_spot["review"] = something
+
+    return make_response(new_spot, 200)
