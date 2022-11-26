4697282d10208f9c16adeb1ceb1afa3c03e5ab3c
lamarjd <jacob.lamar16@gmail.com>
2022-11-26 13:42:50 -0800

    review routes working. CRUD complete on backend

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -8,10 +8,61 @@ from ..models import Spot, User, Review
 
 review_routes = Blueprint('reviews', __name__)
 
-#  get all tasks
-@review_routes.route('/reviews')
+#  get all reviews
+@review_routes.route('/')
 def get_all_reviews():
     # if current_user.is_authenticated:
         reviews = Review.query.all()
         response = {"reviews": [review.to_dict() for review in reviews]}
         return make_response(response, 200)
+
+# get one review
+@review_routes.route('/<int:id>')
+def get_one_review(id):
+    review = Review.query.get(id)
+    return make_response(review.to_dict(), 200)
+
+
+# create a review
+@review_routes.route('/<int:id>/new_review', methods=["POST"])
+def post_review(id):
+    if current_user.is_authenticated:
+        form = NewReview()
+        form['csrf_token'].data = request.cookies['csrf_token']
+        if form.validate_on_submit():
+            review = Review(
+                spot_id = id,
+                user_id = current_user.id,
+                rating = form.data["rating"],
+                body = form.data["body"]
+            )
+        db.session.add(review)
+        db.session.commit()
+        return make_response(review.to_dict(), 201)
+    else: return make_response("Unauthorized", 401)
+
+# Edit a review
+@review_routes.route('/<int:id>', methods=["PUT"])
+def edit_review(id):
+    if current_user.is_authenticated:
+        new_review = Review.query.get(id)
+        form = NewReview()
+        form['csrf_token'].data = request.cookies['csrf_token']
+
+        if form.validate_on_submit():
+            new_review.rating = form.data["rating"]
+            new_review.body = form.data["body"]
+
+        return make_response(new_review.to_dict(), 200)
+
+    else: return make_response("Unauthorized", 401)
+
+@review_routes.route('/<int:id>', methods=["DELETE"])
+def delete_review(id):
+    if current_user.is_authenticated:
+        review = Review.query.get(id)
+        db.session.delete(review)
+        db.session.commit()
+        return make_response("Successfully Deleted", 200)
+    else:
+        return make_response("Unauthorized", 401)
