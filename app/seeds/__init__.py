98ad4405bc3ecfe55b5f567789f5693b1e38a17f
lamarjd <jacob.lamar16@gmail.com>
2022-11-28 13:34:59 -0800

    added conditionals to version file

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -18,9 +18,9 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_spots()
         undo_reviews()
+        undo_spots()
+        undo_users()
     seed_users()
     seed_spots()
     seed_reviews()
@@ -31,7 +31,7 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    undo_spots()
     undo_reviews()
+    undo_spots()
+    undo_users()
     # Add other undo functions here
