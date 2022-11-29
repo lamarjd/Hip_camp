1fb27a200a4e86eeb9624f888fd426560ae6ef0b
lamarjd <jacob.lamar16@gmail.com>
2022-11-28 23:40:59 -0800

    CSS for SplashPage done. Images on Randum sources

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useEffect } from 'react'
 import wakeUp1 from "../../assets/tent-and-view.png"
 import wakeUp2 from "../../assets/tent-view.png"
-import wakeUp3 from "../../assets/wake-up.png"
+// import wakeUp3 from "../../assets/wake-up.png"
+import wakeUp4 from "../../assets/wakeUp4.png"
+import wakeUp5 from "../../assets/wakeUp5.png"
+import wakeUp6 from "../../assets/wakeUp6.png"
+import wakeUp7 from "../../assets/wakeUp7.png"
+import wakeUp8 from "../../assets/wakeUp8.png"
+// import wakeUp9 from "../../assets/wakeUp9.png"
 import "./SplashPage.css"
 
 
@@ -11,47 +18,49 @@ export default function SplashPage() {
     const imgSources = {
         src1: wakeUp1,
         src2: wakeUp2,
-        src3: wakeUp3,
+        // src3: wakeUp3,        
+        src4: wakeUp4,
+        src5: wakeUp5,
+        src6: wakeUp6,        
+        src7: wakeUp7,
+        src8: wakeUp8,
+        // src9: wakeUp9,
     }
-
-    
-    
+   
     function getRandom() {
-        let sourcesValues = Object.values(imgSources);
+        let srcVals = Object.values(imgSources);
         // console.log(sourcesValues)
         // let sourcesKeys = Object.keys(imgSources);
         // console.log(sourcesKeys)
-
         
         // return sourcesValues[Math.floor(Math.random() * sourcesValues.length)] 
 
-        return sourcesValues[Math.floor(Math.random() * sourcesValues.length)]
+        return srcVals[Math.floor(Math.random() * srcVals.length)]
+    
     }
 
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            getRandom()
+        }, 3000)
+
+        return () => clearTimeout(timeout)
+    }, [])
+
+
     // console.log("Random src", getRandom(imgSources))
     // console.log("Random src", getRandom(imgSources))
     // console.log("Random src", getRandom(imgSources))
-
-    
-
-
-
-
-    // console.log(Math.random(sources.length).toFixed(2))
-
-    // sources.map(src => {
-    //     console.log(src)
-    // })
-
-
-    // randomPic(imgSources);
 
   return (
     <div className="splash-container">
         <div className="splash-words">
             <h1>Find yourself outside.</h1>
         <div>
-            <h3>Discover and book tent camping, RV parks, cabins, treehouses, and glamping.</h3>
+            <div className="wrap">
+
+            <h2 id="wrap">Discover and book tent camping, RV <br/>parks, cabins, treehouses, and glamping.</h2>
+            </div>
         </div>
         </div>
 
