2f8f7925254020ca2460c6c85de81f6bc25a6b94
lamarjd <jacob.lamar16@gmail.com>
2022-12-04 22:33:35 -0800

    Final touchups

--- react-app/src/components/Spots/AllSpots.js
+++ react-app/src/components/Spots/AllSpots.js
@@ -40,25 +40,19 @@ function AllSpots() {
 
   return (
     <div className="all-spots-container">
-      {/* <CreateSpotForm />       */}
+  
       <div className="options-container">
         <span className="single-option">
-          {/* <h3>Campsites</h3>
-          <h3>Lodging</h3> */}
-        <h3>{sessionUser && 
-        
-        <CreateSpotFormModal />}</h3>
+
+          <h3>{sessionUser && <CreateSpotFormModal />}</h3>
         </span>
       </div>
       <div className="spot-list">
-
         {spots?.map((spot) => (
           <div key={spot.id} className="single-spots-container">
             <NavLink key={spot?.id} to={`/spots/${spot?.id}`}>
               <div className="all-spot-img">
                 <img
-                  // onMouseOver={handleMouseOver}
-                  // onMouseOut={handleMouseOut}
                   id="spot-pic"
                   alt="spot-pic"
                   src={spot?.imageUrl}
@@ -71,14 +65,6 @@ function AllSpots() {
               </div>
             </NavLink>
             <br />
-
-            {/* <div className="delete-spot">
-              {sessionUser?.id === spot.user_id && (
-                <button onClick={() => dispatch(deleteSpotThunk(spot.id))}>
-                  Delete
-                </button>
-              )}
-            </div> */}
           </div>
         ))}
       </div>
