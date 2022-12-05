df3233e196f1554ae31ce7ac3c21336a75603814
lamarjd <jacob.lamar16@gmail.com>
2022-12-04 16:03:00 -0800

    about me links working

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -6,7 +6,9 @@ export default function Footer() {
     <div>
         <div className="about-me">
             <h6>About the Developer</h6>
-          <i className="fa-brands fa-github"></i>
+          <i onClick={(e) => (window.location = 'https://github.com/lamarjd/Hip_camp')}className="fa-brands fa-github"></i>
+
+          <p onClick={(e) => (window.location = 'https://www.linkedin.com/in/jacob-lamar-73449040/')}>LinkedIn</p>
         </div>
     </div>
   )
