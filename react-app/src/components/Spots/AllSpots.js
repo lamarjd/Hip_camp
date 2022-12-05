e6c5784e14ffbca4ce66e04bf48f9f0278a5eb77
lamarjd <jacob.lamar16@gmail.com>
2022-12-05 00:07:56 -0800

    Trying to get reviews to show on each page for AllApots

--- react-app/src/components/Spots/AllSpots.js
+++ react-app/src/components/Spots/AllSpots.js
@@ -1,22 +1,52 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink, useHistory } from "react-router-dom";
+import { NavLink, useHistory, useParams } from "react-router-dom";
 import CreateSpotFormModal from "../Modals/CreateSpotFormModal";
 import { fetchSpots, editSpotThunk, deleteSpotThunk } from "../../store/spot";
+import { getAllReviews } from "../../store/review";
 import defaultPic from "../../assets/wake-up.png";
 import "./Spots.css";
 
 function AllSpots() {
   const dispatch = useDispatch();
+  const { spotId } = useParams();
+  
+  useEffect(() => {
+    dispatch(fetchSpots());
+    dispatch(getAllReviews());
+  }, [dispatch]);
 
+  const rev = useSelector((state) => Object.values(state.reviews));
+  console.log("rev", rev)
+  
   const spots = useSelector((state) => Object.values(state.spots));
   console.log("SPOTS", spots);
+  
+  // const revBySpot = (spots) => {
+  //   let arr =[]
+
+  //   for (let i = 0; i < spots.length - 1; i++) {
+  //     // console.log("spots[i]", spots[i])
+  //     if (spots[i] === rev.spot_id) {
+  //       arr.push(rev.rating)
+  //   }
+  //   }
+  //   return arr
+  // };
+
+  // console.log("review rating by spot", revBySpot(spots))
+
+
+
+const spotReview = rev.map((review) => {
+  return "Rev rating --->" + review.rating + "rev spot_ID" + review.spot_id
+})
+console.log("spot reviews", spotReview)
+  
+
 
   const sessionUser = useSelector((state) => state.session.user);
 
-  useEffect(() => {
-    dispatch(fetchSpots());
-  }, [dispatch]);
 
   // let filteredSpots = spots.filter((spot) => {
   //   return spot.type === "Campsite";
@@ -37,6 +67,26 @@ function AllSpots() {
   // const changeBackground = (e) => {
   //   e.target.style.background = 'red';
   // }
+
+  // const filteredReviews = rev.filter((review) => review.spot_id === +spotId);
+  // console.log("Filtered Reviews from All Spots", filteredReviews)
+
+  // ******** Rating Percentage CALCS ***************
+  // const reviews = filteredReviews.map((review) => {
+  //   return review.rating;
+  // });
+
+  // console.log("reviews", reviews.reduce((accum, num) => {
+  //   return accum + num;
+  // }, 0))
+
+  // const revPercent = reviews.reduce((accum, num) => {
+  //   let sum = accum + num;
+  //   let avg = 100 - sum / 5 / reviews.length;
+  //   return Math.floor(avg);
+  // }, 0);
+
+  // *************************************************
 
   return (
     <div className="all-spots-container">
@@ -68,6 +118,11 @@ function AllSpots() {
                 />
 
                 <h1>{spot?.name}</h1>
+                <p>{}</p>
+
+
+                  <p>%</p>
+
               </div>
             </NavLink>
             <br />
